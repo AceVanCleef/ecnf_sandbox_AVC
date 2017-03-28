f03957eb0d2a8f269bc35700db8f36c2cd19dc77
Stefan Wohlgensinger <stefan.wohlgensinger@students.fhnw.ch>
2017-03-28 10:56:24 +0200

    initial commit

---
+++ ecnfSandbox/ecnfSandboxLib/WhyDelegates.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ecnfSandboxLib
+{
+    /// <summary>
+    /// Source: https://www.youtube.com/watch?v=rxLNJ8jCN1c
+    /// </summary>
+    public class WhyDelegates
+    {
+        //define the delegate (?)
+
+        //The method using the delegate
+
+        //delegateable methods:
+        private bool GreaterThanTen(int x)
+        {
+            return x > 10;
+        }
+
+        private bool LessThanTen(int x)
+        {
+            return x < 10;
+        }
+
+        private bool EqualsTen(int x)
+        {
+            return x == 10;
+        }
+
+        private bool IsMultipleOfTen(int x)
+        {
+            return x % 10 == 0;
+        }
+    }
+}
