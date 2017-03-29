f244b7402fc6b25d3db620a57a5ac56e0320e9be
Stefan Wohlgensinger <stefan.wohlgensinger@students.fhnw.ch>
2017-03-29 20:18:47 +0200

    chained delegates experiment 01

--- ecnfSandbox/ecnfSandboxConsole/Program.cs
+++ ecnfSandbox/ecnfSandboxConsole/Program.cs
@@ -13,22 +13,61 @@ namespace ecnfSandboxConsole
         static void Main(string[] args)
         {
             Console.WriteLine("hello world");
+            /* Executing experiments: */
+            ExecuteWhyDelegatesExperiments();
+            ExecuteChainedDelegatesExperiments();
 
+            Console.ReadKey();
+
+        }
+
+        public static bool OnlyOfLengthThree(string s)
+        {
+            return s.Length == 3;
+        }
+
+
+        private static void ExecuteChainedDelegatesExperiments()
+        {
+            ChainedDelegates delegateObj = new ChainedDelegates(5, "Tim");
+            int calcResult = delegateObj.doMaths(5);
+            string concatResult = delegateObj.doConcatination("Tim");
+
+            Console.WriteLine($"\n[Executing Chaineddelegates Experiment]");
+            Console.WriteLine($"using 'int doMaths(int x)' chained delegate: Result = {calcResult}");
+            Console.WriteLine($"using 'string doConcatination(string s)' chained delegate: Result = {concatResult}");
+            Console.WriteLine("Exiting ChainedDelegates Experiment \n");
+
+            /* Fazit:
+             * Nur das return der zuletzt angehängten Methode wird vom delegate zurückgegeben.
+             * 
+             * Frage:
+             * Gibt es ein Use Case, in welchem es sinnvoll ist, verkettete delegates zu benutzen?
+             * Dabei sollten alle dem delegate angehängten Methoden ausgeführt werden.
+             **/
+        }
+
+        private static void ExecuteWhyDelegatesExperiments()
+        {
+            Console.WriteLine("\n [Executing WhyDelegates Experiments]");
             /********** A) WhyDelegates *************/
-            Console.WriteLine("WhyDelegates' results: ");
+            Console.WriteLine("WhyDelegates results: ");
             int[] numbers = new[] { 5, 15, 10, 25, 8, 20 };
+
 
             /********** B) Exeriment mit externer Methodenreferenzzuweisung *************/
             //Who is the delegate named gauntlet?
             /* Braucht es wohl gar nicht? Könnte direkt WhyDelegates.GreaterThanTen als Param. übergeben */
-            WhyDelegates.MeDelegate gauntlet = WhyDelegates.GreaterThanTen; 
+            WhyDelegates.MeDelegate gauntlet = WhyDelegates.GreaterThanTen;
             IEnumerable<int> result = WhyDelegates.ConsumeDelegate(numbers, gauntlet);
             foreach (int n in result)
             {
-                Console.WriteLine(n);
+                Console.WriteLine($"Using local 'gauntlet = ...' and Foo(numbers, gauntlet): {n}");
             }
             /* Fazit: Machbar, aber wo macht dieser Fall überhaupt Sinn? Hm, vielleicht 
-             * bei mehrfacher wiederverwendung von gauntlet.*/
+             * bei mehrfacher wiederverwendung von gauntlet.
+             **/
+
 
             /********** C)WhyDelegatesWithObjects: Experimente mit Objekten *************/
             Console.WriteLine("WhyDelegatesWithObject result:");
@@ -38,17 +77,22 @@ namespace ecnfSandboxConsole
             IEnumerable<string> _myStringsL3 = delegateObj.GetNamesOfLength3(OnlyOfLengthThree);
             foreach (string n in _myStringsL3)
             {
-                Console.WriteLine(n);
+                Console.WriteLine($"using local static function as parameter [foo(localStatic)]: {n}");
             }
             /* Fazit: Egal woher, eine Methode kann von beliebig wo übergeben werden.
              *        Wichtig nur, es muss den geforderte Rückgabewerte und 
-             *        die Parameterliste erfüllen (siehe if(gauntlet(string))*/
+             *        die Parameterliste erfüllen (siehe if(gauntlet(string))
+             **/
+
+
             Console.WriteLine("----");
+
+
             // C) - 2: Delegate = MethodXY innerhalb der Objektmethode.
             IEnumerable<string> _myStringsL4 = delegateObj.GetNamesOfLength4();
             foreach (string n in _myStringsL4)
             {
-                Console.WriteLine(n);
+                Console.WriteLine($"When delegate was initialized locally within method: {n}");
             }
             /* Fazit: 
              * Delegatezuweisungen können auch innerhalb einer anderswo 
@@ -56,6 +100,9 @@ namespace ecnfSandboxConsole
              * Vielleicht etwas schlecht für die Lesbarkeit. Man sieht nicht,
              * was geschieht. */
             Console.WriteLine("----");
+
+
+
 
             /**** D) Trying: array -> filtered IEnumerable<string> + direct method call by reference ****/
             IEnumerable<string> CollectionByLinq = names.Where(OnlyOfLengthThree);
@@ -66,20 +113,17 @@ namespace ecnfSandboxConsole
             /* Fazit: Funktionsreferenz kann direkt übergeben werden *
 
             /**** E) Objektmethode als Parameter ****/
-            /* Gehen nur statische Funktionen? Können auch Objektmethoden übergeben werden? Fazit: Objektmethoden gehen auch. */
             IEnumerable<string> namesWithLenghtOfFive = delegateObj.GetNamesOfLength3(delegateObj.LenghtEqualsFive);
-            foreach(string name in namesWithLenghtOfFive)
+            foreach (string name in namesWithLenghtOfFive)
             {
-                Console.WriteLine($"Names filtered by Objektmethod as Param: {name} by lenght of {name.Length}");
+                Console.WriteLine($"Names filtered by Objektmethod as Param [myObj.foo(otherObj.method)]: {name} by lenght of {name.Length}");
             }
+            /* Gehen nur statische Funktionen? Können auch Objektmethoden 
+             * übergeben werden?   
+             * Fazit: Objektmethoden gehen auch. 
+             **/
 
-            Console.ReadKey();
-
-        }
-
-        public static bool OnlyOfLengthThree(string s)
-        {
-            return s.Length == 3;
+            Console.WriteLine("Exiting WhyDelegatesExperiments \n");
         }
     }
 }
---
+++ ecnfSandbox/ecnfSandboxLib/ChainedDelegates.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ecnfSandboxLib
+{
+    public class ChainedDelegates
+    {
+        public int Number { get; set; }
+        public string Astring { get; set; }
+
+        //delegates
+        public delegate int ChainedCalculation(int x);
+        public delegate string ChainedConcatination(string s);
+        public ChainedCalculation doMaths;
+        public ChainedConcatination doConcatination;
+
+
+        public ChainedDelegates (int number, string str)
+        {
+            Number = number;
+            Astring = str;
+            doMaths += MultiplyBy5;
+            doMaths += AddThree;
+            doConcatination += ConcatHello;
+            doConcatination += ConcatHowAreYou;
+        }
+
+        public int MultiplyBy5(int x)
+        {
+            return x * 5;
+        }
+
+        public int AddThree(int x)
+        {
+            return x + 3;
+        }
+
+        public string ConcatHello(string name)
+        {
+            return "Hello " + name;
+        }
+
+        public string ConcatHowAreYou(string moment)
+        {
+            return "how are you feeling " + moment;
+        }
+    }
+}
